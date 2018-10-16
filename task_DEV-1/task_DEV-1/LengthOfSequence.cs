0829b6910e038969e845986ddbff6e60e06321cc
Egor Solovyev <egorsolovyev98@gmail.com>
2018-10-17 00:13:23 +0300

    Check the uniqueness of characters

--- task_DEV-1/task_DEV-1/LengthOfSequence.cs
+++ task_DEV-1/task_DEV-1/LengthOfSequence.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace task_DEV1
 {
@@ -6,28 +7,37 @@ namespace task_DEV1
     {
         public static int GetLengthOfSequence(this string sequence)
         {
-            int maxLength = 0;
-            int counter = 1;
-
             if (sequence.Length == 1)
             {
                 return 1;
             }
 
+            int maxLength = 0;
+            int counter = 1;
+            HashSet<char> set = new HashSet<char>();
+
             for (int i = 1; i < sequence.Length; i++)
             {
                 if (sequence[i - 1] != sequence[i])
                 {
+                    set.Add(sequence[i - 1]);
                     counter++;
+
+                    if(set.Contains(sequence[i])) //check the unique of character 
+                    {
+                        counter--;
+                    }
+
+                    if (counter > maxLength)
+                    {
+                        maxLength = counter;
+                    }
+
                 }
                 else
                 {
                     counter = 1;
-                }
-
-                if (counter > maxLength)
-                {
-                    maxLength = counter;
+                    set.Clear();
                 }
             }
 
