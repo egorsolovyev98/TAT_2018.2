83e1c464d81fdecd23c6c9cfb23f2fc2738eaa60
Egor Solovyev <egorsolovyev98@gmail.com>
2018-10-20 12:23:23 +0300

    First version

---
+++ lab_Stack/MyStack/MyStack/MyStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyStack
+{
+    class MyStack
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Stack<int> myStack = new Stack<int>(10);
+
+                myStack.Push(10);
+                myStack.Push(14);
+                myStack.Push(15);
+
+                myStack.Pop();
+                myStack.Pop();
+
+                Console.WriteLine(myStack.Pop());
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
