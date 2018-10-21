bfa8839d981c6bfd3a61a2855626495d1919ed5a
Egor Solovyev <egorsolovyev98@gmail.com>
2018-10-21 15:54:55 +0300

    Code style

--- MyStack/MyStack/MyStack.cs
+++ MyStack/MyStack/MyStack.cs
@@ -2,8 +2,15 @@
 
 namespace MyStack
 {
+    /// <summary>
+    /// My stack.
+    /// </summary>
     class MyStack
     {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
             try
