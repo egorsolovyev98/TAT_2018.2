24eda84e785b1d08196ee9dc061dc441c828a4bb
Egor Solovyev <egorsolovyev98@gmail.com>
2018-10-24 15:04:37 +0300

    Sketch of solution

---
+++ task_DEV-4/task_DEV-4/EntryPoint.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task_DEV4
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception("wrong numer of arguments.");
+                }
+
+                string path = args[0];
+                XmlParser parser = new XmlParser(path);
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
---
+++ task_DEV-4/task_DEV-4/XmlParser.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace task_DEV4
+{
+    /// <summary>
+    /// Xml parser.
+    /// </summary>
+    public class XmlParser
+    {
+        private string filePath;
+        private Queue<StringBuilder> xmlElements = new Queue<StringBuilder>();
+
+        public XmlParser(string filePath)
+        {
+            this.filePath = filePath;
+            fileReader();
+        }
+
+        private void fileReader()
+        {
+            using (StreamReader reader = new StreamReader(filePath, System.Text.Encoding.UTF8))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("?xml"))
+                    {
+                        continue;
+                    }
+
+                    if(!line.Contains("</"))
+                    {
+                        StringBuilder str = new StringBuilder(line.Remove(line.IndexOf("<"), line.IndexOf(">")));
+                    }
+                }
+            }
+        }
+    }
+}
