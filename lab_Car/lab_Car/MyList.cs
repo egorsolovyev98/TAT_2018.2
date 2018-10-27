ed0c78f544f1e31ca0e65a84c824d1bb27679fbf
Egor Solovyev <egorsolovyev98@gmail.com>
2018-10-27 15:40:54 +0300

    Cosmetics

--- lab_Car/lab_Car/MyList.cs
+++ lab_Car/lab_Car/MyList.cs
@@ -1,15 +1,40 @@
-﻿using System;
-
-namespace lab_Car
+﻿namespace lab_Car
 {
+    /// <summary>
+    /// My list.
+    /// </summary>
     public class MyList
     {
+        /// <summary>
+        /// Node.
+        /// </summary>
         public class Node
         {
+            /// <summary>
+            /// Gets or sets the car.
+            /// </summary>
+            /// <value>The car.</value>
             public Car car { get; set; }
+
+
+            /// <summary>
+            /// Gets or sets the previous.
+            /// </summary>
+            /// <value>The previous.</value>
             public Node prev { get; set; }
+
+
+            /// <summary>
+            /// Gets or sets the next.
+            /// </summary>
+            /// <value>The next.</value>
             public Node next { get; set; }
 
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="T:lab_Car.MyList.Node"/> class.
+            /// </summary>
+            /// <param name="car">Car.</param>
             public Node(Car car)
             {
                 this.car = car;
@@ -18,13 +43,27 @@ namespace lab_Car
             }
         }
 
+
+        /// <summary>
+        /// Gets or sets the head.
+        /// </summary>
+        /// <value>The head.</value>
         public Node head { get; set; }
 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:lab_Car.MyList"/> class.
+        /// </summary>
         public MyList()
         {
             head = null;
         }
 
+
+        /// <summary>
+        /// Adds element to the head.
+        /// </summary>
+        /// <param name="car">Car.</param>
         public void AddHead(Car car)
         {
             if (head == null)
@@ -40,11 +79,17 @@ namespace lab_Car
             }
         }
 
+
+        /// <summary>
+        /// Search the specified car.
+        /// </summary>
+        /// <returns>The MyList with cars.</returns>
+        /// <param name="car">Car.</param>
         public MyList Search(Car car)
         {
             Node tmpHead = head;
-
             MyList list = new MyList();
+
             while(tmpHead != null)
             {
                 if (car.Compare(tmpHead.car))
@@ -57,6 +102,5 @@ namespace lab_Car
 
             return list;
         }
-
     }
 }
