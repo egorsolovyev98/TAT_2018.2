7058585eaa1dc10c2902ee3f6642c6f00438dc46
Egor Solovyev <egorsolovyev98@gmail.com>
2018-10-27 15:39:01 +0300

    Working solution

---
+++ lab_Car/lab_Car/EntryPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab_Car
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                MyList list = new MyList();
+
+                list.AddHead(new Car("BMW", "X5", "Black"));
+                list.AddHead(new Car("BMW", "X6", "White"));
+                list.AddHead(new Car("Mazda", "6", "Red"));
+                list.AddHead(new Car("Pagani", "Huaira", "Yellow"));
+
+                MyList searchList = list.Search(new Car("BMW", "5", "Green"));
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
---
+++ lab_Car/lab_Car/MyList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace lab_Car
+{
+    public class MyList
+    {
+        public class Node
+        {
+            public Car car { get; set; }
+            public Node prev { get; set; }
+            public Node next { get; set; }
+
+            public Node(Car car)
+            {
+                this.car = car;
+                prev = null;
+                next = null;
+            }
+        }
+
+        public Node head { get; set; }
+
+        public MyList()
+        {
+            head = null;
+        }
+
+        public void AddHead(Car car)
+        {
+            if (head == null)
+            {
+                head = new Node(car);
+            }
+            else
+            {
+                Node insertNode = new Node(car);
+                head.prev = insertNode;
+                insertNode.next = head;
+                head = insertNode;
+            }
+        }
+
+        public MyList Search(Car car)
+        {
+            Node tmpHead = head;
+
+            MyList list = new MyList();
+            while(tmpHead != null)
+            {
+                if (car.Compare(tmpHead.car))
+                {
+                    list.AddHead(car);
+                }
+
+                tmpHead = tmpHead.next;
+            }
+
+            return list;
+        }
+
+    }
+}
