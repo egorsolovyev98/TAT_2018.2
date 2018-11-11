849691601f225abe89fc51c6d185c9c60ce0783b
Egor Solovyev <egorsolovyev98@gmail.com>
2018-11-11 17:12:15 +0300

    Entry point modified

--- task_DEV-6/task_DEV-6/EntryPoint.cs
+++ task_DEV-6/task_DEV-6/EntryPoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace task_DEV6
@@ -23,22 +24,28 @@ namespace task_DEV6
 
                 foreach(string filePath in args)
                 {
+                    string newExtension = string.Empty;
+                    List<string> file = null;
+
                     if (Path.GetExtension(filePath).Equals(".xml"))
                     {
                         XmlToJsonParser xmlToJsonParser = new XmlToJsonParser(filePath);
                         xmlToJsonParser.ReadFile();
-
-                        FileWritter fileWritter = new FileWritter(filePath, xmlToJsonParser.Parse());
-                        fileWritter.ChangeFileExtension("json");
-                        fileWritter.WriteFile();
+                        file = xmlToJsonParser.Parse();
+                        newExtension = "json";
                     }
                     else if (Path.GetExtension(filePath).Equals(".json"))
                     {
                         JsonToXmlParser jsonToXmlParser = new JsonToXmlParser(filePath);
                         jsonToXmlParser.ReadFile();
+                        file = jsonToXmlParser.Parse();
+                        newExtension = "xml";
+                    }
 
-                        FileWritter fileWritter = new FileWritter(filePath, jsonToXmlParser.Parse());
-                        fileWritter.ChangeFileExtension("xml");
+                    if ((newExtension != string.Empty) && (file != null))
+                    {
+                        FileWritter fileWritter = new FileWritter(filePath, file);
+                        fileWritter.ChangeFileExtension(newExtension);
                         fileWritter.WriteFile();
                     }
                 }
