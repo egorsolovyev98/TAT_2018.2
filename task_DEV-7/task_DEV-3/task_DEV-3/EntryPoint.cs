c1c78a007a9b85ef024a37cbe36e84356d6430fd
Egor Solovyev <egorsolovyev98@gmail.com>
2018-11-23 09:50:35 +0300

    Working solution

---
+++ task_DEV-7/task_DEV-3/task_DEV-3/EntryPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace task_DEV3
+{
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    class EntryPoint
+    {
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 2)
+                {
+                    throw new Exception("Wrong number of arguments.");
+                }
+
+                int inputValue = Convert.ToInt32(args[0]);
+                int newRadix = Convert.ToInt32(args[1]);
+
+                const int MinRadixByCondition = 2;
+                const int MaxRadixByCondition = 20;
+
+                if (newRadix < MinRadixByCondition || newRadix > MaxRadixByCondition)
+                {
+                    throw new Exception("Wrong radix.");
+                }
+                string inputValueInNewRadix = inputValue.ToNewRadix(newRadix);
+
+                Console.WriteLine($"Input value = {inputValue} ,radix = 10.");
+                Console.WriteLine($"Input value = {inputValueInNewRadix} ,radix = {newRadix}.");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error: " + e.Message);
+            }
+        }
+    }
+}
