f7b56b12a41ffc33ea4db7813e3d22454e48d7a3
Egor Solovyev <egorsolovyev98@gmail.com>
2018-12-08 10:35:27 +0300

    First task added

---
+++ EqualCharactersInString/EqualCharactersInString/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EqualCharactersInString
+{
+    class EntryPoint
+    {
+        public static void Main(string[] args)
+        {
+            string inputString = Console.ReadLine();
+
+            EqualCharactersFinder equalCharactersFinder = new EqualCharactersFinder();
+            equalCharactersFinder.FindNumberOfEqualCharacters(inputString);
+            equalCharactersFinder.PrintResult();
+        }
+    }
+}
---
+++ EqualCharactersInString/EqualCharactersInString/EqualCharactersFinder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualCharactersInString
+{
+    public class EqualCharactersFinder
+    {
+        public Dictionary<char, int> Dictionary { get; set; }
+
+        public EqualCharactersFinder()
+        {
+            Dictionary = new Dictionary<char, int>();
+        }
+
+        public Dictionary<char,int> FindNumberOfEqualCharacters(string str)
+        {
+            Dictionary.Clear();
+
+            foreach(var i in str)
+            {
+                if(!Dictionary.ContainsKey(i))
+                {
+                    Dictionary.Add(i, 1);
+                }
+                else
+                {
+                    Dictionary[i]++;
+                }
+            }
+
+            return Dictionary;
+        }
+
+        public void PrintResult()
+        {
+            foreach(var i in Dictionary)
+            {
+                Console.WriteLine($"{i.Key} - {i.Value}");
+            }
+        }
+    }
+}
