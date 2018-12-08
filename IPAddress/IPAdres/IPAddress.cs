1e420c15de0e97630f501ba18b4840ee8b7aa392
Egor Solovyev <egorsolovyev98@gmail.com>
2018-12-08 11:52:14 +0300

    task2 added

---
+++ IPAddress/IPAdres/IPAddress.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+
+namespace IPAdres
+{
+    public class IPAddress
+    {
+        public string GetIPAddress(string str)
+        {
+            StringBuilder ipAddress = new StringBuilder();
+            int startIndex = 0;
+            int endIndex = 0;
+            int countOfPoints = 0;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!char.IsDigit(str[i]))
+                {
+                    continue;
+                }
+
+                startIndex = i;
+
+                if (countOfPoints != 3)
+                {
+                    endIndex = str.IndexOf('.', startIndex);
+                    countOfPoints++;
+                }
+                else
+                {
+                    endIndex = startIndex;
+
+                    while (char.IsDigit(str[endIndex]))
+                    {
+                        endIndex++;
+                    }
+
+                    endIndex--;
+                }
+
+
+                if (endIndex - startIndex > 3)
+                {
+                    return GetIPAddress(str.Substring(endIndex));
+                }
+
+                ipAddress.Append(str.Substring(startIndex, endIndex - startIndex + 1));
+                i = endIndex;
+            }
+
+            string resultIpAddress = ipAddress.ToString();
+
+            return IsValid(resultIpAddress) ? resultIpAddress : string.Empty;
+        }
+
+        public bool IsValid (string ipAddress)
+        {
+            string[] numbers = ipAddress.Split('.');
+
+            foreach (var i in numbers)
+            {
+                int number = Convert.ToInt32(i);
+
+                if (number < 0 || number > 255)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ IPAddress/IPAdres/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IPAdres
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string str = "a123.123.123.123ojhgf";
+                var ipAddress = new IPAddress();
+
+                Console.WriteLine(ipAddress.GetIPAddress(str));
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.Message);
+            }
+        }
+    }
+}
